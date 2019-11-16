98762f05c7b409a685a2d27dc1c899ea9e03471a
RedLavaRus <github@redlava.ru>
2019-11-16 23:04:45 +0300

    Верстка

--- index.php
+++ index.php
@@ -45,9 +45,9 @@ $('a[href*="#"]').click(function() {
 			height: 70px;
 		}
 		.logo_box img{
-			background: #ff3333;
-			margin: 3px 15px 3px 15px;
+			margin: 6px 2px 10px 20px;
 			float: left;
+			width: 50px;
 		}
 		.logo_text{
 			margin: 15px 20px;
@@ -56,6 +56,10 @@ $('a[href*="#"]').click(function() {
 			padding: 0 30px;
 			font-family: 'Yeon Sung', cursive;
 
+		}
+		#logo1{
+			width: 256px;
+			margin:  10vh 350px 0 350px;
 		}
 		.line_n_1_l2{
 			width: calc(100% - 510px);
@@ -110,7 +114,7 @@ $('a[href*="#"]').click(function() {
 			background-size: 100% 100%;
 		}
 		.smal_screen_1_s1{
-			margin: 220px 0 0 0;
+			margin: 20px 0 0 0;
 			font-size: 48px;
 			text-align: center;
 			font-family: 'Yeon Sung', cursive;
@@ -201,13 +205,81 @@ body::-webkit-scrollbar-thumb
 	font-size: 24px;
 	padding: 20px 0 ;
 }
+.blok_vert_trade{
+	width: 1000px;
+	margin: 0 auto;
+}
+.blok_l1{
+	width: 312px;
+	border: 1px dotted red;
+	margin: 20px 10px;
+	float: left;
+	color:#fff;
+}
+.blok_l1:hover{
+	width: 312px;
+	border: 1px dotted red;
+	margin: 20px 10px;
+	float: left;
+	color:#fff;
+	background:#191515;
+    box-shadow: 0 0 40px rgba(101,0,0,0.5);
+}
+.tp_name{
+	width: 100%;
+	height: 24px;
+	text-align: center;
+	color: #fff;
+	background-color: #331515;
 
+	border-bottom: 1px dotted red;
+	padding: 10px 0;
+	font-size: 18px;
+	font-weight: 900;
+}
+.tp_text{
+	height: 500px;
+}
+.tp_bye{
+	width: 100%;
+	height: 24px;
+	text-align: center;
+	color: #fff;
+	background-color: #331515;
+	border-top: 1px dotted red;
+	padding: 10px 0;
+	font-size: 18px;
+	font-weight: 900;
+}
+.info_bye_dop{
+	width: 983px;
+	margin: 0 auto;
+	text-align: center;
+	color: #fff;
+	background-color: #331515;
+	border: 1px dotted red;
+	padding: 10px 0;
+	font-size: 18px;
+	font-weight: 900;
+}
+.info_bye_dop:hover{
+	background:#401515;
+    box-shadow: 0 0 40px rgba(101,0,0,0.5);
+}
+#h2l1{
+	text-align: center;
+	color: #fff;
+	font-size: 32px;
+	margin: 85px 0 0 0;
+}
 	</style>
+
+
 </head>
 <body>
 <div class="line_n_1">
 	<div class="logo_box">
-			<img src ="/RedCore/img/logo/logo64x64.png">
+			<img src ="/RedCore/img/logo/logo_red_64.png">
 			<div class="logo_text">REDLAVA</div>
 	</div>
 	<div class="line_n_1_l2">
@@ -222,6 +294,7 @@ body::-webkit-scrollbar-thumb
 <div class="screen" id="sl1">
 	<br>
 	<div class="smal_screen_1">
+		<img src="/RedCore/img/logo/logo_red_512.png" id="logo1">
 		<div class="smal_screen_1_s1">RED LAVA</div><BR>
 		<div class="smal_screen_1_s2">МЫ СОЗДАЕМ ИНСТРУМЕНТ ДЛЯ ПРИВЛЕЧЕНИЯ КЛИЕНТОВ</div>
 		<div class="left_bot_sl1_x1">БЕСПЛАТНАЯ КОНСУЛЬТАЦИЯ</div>
@@ -306,8 +379,242 @@ body::-webkit-scrollbar-thumb
 	<div class="redline_phone">
 		Позвони +7 (960) 49 44 999 и получи уникальное предложение!
 	</div>
+	<h2>Тарифные планы на создание веб сайтов</h2>
+	<div class="blok_vert_trade">
+		<div class="blok_l1">
+			<div class="tp_name">
+				Сайт Визитка
+			</div>
+			<div class="tp_text">
+				blabla
+			</div>
+			<div class="tp_bye">
+				ВЫБРАТЬ
+			</div>
+		</div>
+		<div class="blok_l1">
+			<div class="tp_name">
+				Блог
+			</div>
+			<div class="tp_text">
+				blabla
+			</div>
+			<div class="tp_bye">
+				ВЫБРАТЬ
+			</div>
+		</div>
+			
+		</div>
+		<div class="blok_l1">
+			<div class="tp_name">
+				Лендинг
+			</div>
+			<div class="tp_text">
+				blabla
+			</div>
+			<div class="tp_bye">
+				ВЫБРАТЬ
+			</div>
+		</div>
+		<div class="clear"></div>
+						<div class="info_bye_dop">
+				ОТКРЫТЬ ВСЕ ТАРИФНЫЕ ПЛАНЫ
+			</div>
+		</div>
+	</div>
+		<div class="clear"></div><br><br><br>
+	<h2 id="h2l1">Продвижение? Это к нам.</h2>
 
 </div>
+<div class="clear"></div><br><br><br><div class="clear"></div>
+	<div class="main_paje_line">
+		<div class="main_paje_line_l1_z1"> </div>
+		<div class="main_paje_line_r1_z1">
+			<br><br>
+			<h3>Прозрачная отчетность.</h3>
+			<p><br>
+				Мы не вешаем лапшу науши, что какие-то работы проводятся. У нас полностью прозрачные отчеты по выполненной работе. Вы всегда знаете, за что имеено платите.
+				
+
+			</p>
+		</div>
+	</div>
+<div class="clear"></div><br><br><br><div class="clear"></div>
+	<div class="main_paje_line">
+		<div class="main_paje_line_l1_z1">
+
+			<br><br>
+			<h3>Закрываем максимальное количество клиентов!</h3>
+			<p><br>
+				Сематическое ядро в 10-20 ключевых слов? Нет, мы закрываем нишу полностью. Количество ключевых слов измеряется в сотнях <br><br>
+				Мы против того, что бы клиенты уходили к вашим конкурентам.
+				
+
+			</p>
+			 </div>
+		<div class="main_paje_line_r1_z1">
+		</div>
+	</div>
+	<div class="clear"></div><br><br><br><div class="clear"></div>
+	<div class="main_paje_line">
+		<div class="main_paje_line_l1_z1"> </div>
+		<div class="main_paje_line_r1_z1">
+			<br><br>
+			<h3>Два фронта - не наш путь</h3>
+			<p><br>
+				Если мы работаем свами, то мы не продвигаем ваших конкурентов. Это разве честно? Брать деньги с клиента, а еще уводить его клиентов к конкуренту? Мы считаем, что нет! И наше решение окончательное.
+
+				
+
+			</p>
+		</div>
+	</div>
+<div class="clear"></div><br><br><br><div class="clear"></div>
+	<div class="main_paje_line">
+		<div class="main_paje_line_l1_z1">
+
+			<br><br>
+			<h3>Оплата по факту.</h3>
+			<p><br>
+				Мы берем всего 30% предоплаты. Остальное в процессе работы.<br><br>
+				Мы уверены, что довольный клиент, всегда готов заплатить. А иногда даже больше чем договаривались. 
+				
+
+			</p>
+			 </div>
+		<div class="main_paje_line_r1_z1">
+		</div>
+	</div>
+
+
+</div>
+<div class="clear"></div><br><br><br><div class="clear"></div>
+
+<div class="screen">
+	<h2>Вам мало? А это еще не все!</h2>
+	<div class="redline_phone">
+		Позвони +7 (960) 49 44 999 Пора наращивать клиентскую базу!
+	</div>
+	<h2>Тарифные планы на продвижение веб сайтов</h2>
+	<div class="blok_vert_trade">
+		<div class="blok_l1">
+			<div class="tp_name">
+				Мини 
+			</div>
+			<div class="tp_text">
+				blabla
+			</div>
+			<div class="tp_bye">
+				ВЫБРАТЬ
+			</div>
+		</div>
+		<div class="blok_l1">
+			<div class="tp_name">
+				Стандарт
+			</div>
+			<div class="tp_text">
+				blabla
+			</div>
+			<div class="tp_bye">
+				ВЫБРАТЬ
+			</div>
+		</div>
+			
+		</div>
+		<div class="blok_l1">
+			<div class="tp_name">
+				Оптима
+			</div>
+			<div class="tp_text">
+				blabla
+			</div>
+			<div class="tp_bye">
+				ВЫБРАТЬ
+			</div>
+		</div>
+		<div class="clear"></div>
+						<div class="info_bye_dop">
+				ОТКРЫТЬ ВСЕ ТАРИФНЫЕ ПЛАНЫ
+			</div>
+		</div>
+	</div>
+		<div class="clear"></div><br><br><br>
+	<h2 id="h2l1">Реклама - какой она должна быть.</h2>	
+	<div class="clear"></div><br><br><br><div class="clear"></div>
+	<div class="main_paje_line">
+		<div class="main_paje_line_l1_z1"> </div>
+		<div class="main_paje_line_r1_z1">
+			<br><br>
+			<h3>Прозрачная отчетность.</h3>
+			<p><br>
+				В личном кабинете вы всегда знаете куда и сколько было потрачено. И какой результат.
+				
+
+			</p>
+		</div>
+	</div>
+<div class="clear"></div><br><br><br><div class="clear"></div>
+	<div class="main_paje_line">
+		<div class="main_paje_line_l1_z1">
+
+			<br><br>
+			<h3>Фиксированные ставки.</h3>
+			<p><br>
+				У нас нет скрытых коммисий, и мы всегда прямо и открыто говорим, о нашей коммисии на ведение рекламы.<br><br>
+				К примеру для небольших компаний, где бюджек до 10 000 рублей, наша коммисия составит всего 2000 руб.
+				
+
+			</p>
+			 </div>
+		<div class="main_paje_line_r1_z1">
+		</div>
+	</div>
+	<div class="clear"></div><br><br><br><div class="clear"></div>
+<div class="screen">	
+	<h2>Тарифные планы на рекламные компании</h2>
+	<div class="blok_vert_trade">
+		<div class="blok_l1">
+			<div class="tp_name">
+				Альфа 
+			</div>
+			<div class="tp_text">
+				blabla
+			</div>
+			<div class="tp_bye">
+				ВЫБРАТЬ
+			</div>
+		</div>
+		<div class="blok_l1">
+			<div class="tp_name">
+				Бета
+			</div>
+			<div class="tp_text">
+				blabla
+			</div>
+			<div class="tp_bye">
+				ВЫБРАТЬ
+			</div>
+		</div>
+			
+		</div>
+		<div class="blok_l1">
+			<div class="tp_name">
+				Гамма
+			</div>
+			<div class="tp_text">
+				blabla
+			</div>
+			<div class="tp_bye">
+				ВЫБРАТЬ
+			</div>
+		</div>
+		<div class="clear"></div>
+						<div class="info_bye_dop">
+				ОТКРЫТЬ ВСЕ ТАРИФНЫЕ ПЛАНЫ
+			</div>
+		</div>
+	</div>	
+
 <BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR>	
 </body>
 </html>
